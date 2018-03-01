29070e093cfca40d3f911428bb42abbfa6f2b866
江画 <qijun.qj@alibaba-inc.com>
2018-03-01 15:04:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# tagd [![Build Status](https://api.travis-ci.org/uncledu/genVersion.svg?branch=master)](https://api.travis-ci.org/uncledu/genVersion.svg?branch=master)[![Coverage Status](https://coveralls.io/repos/github/uncledu/genVersion/badge.svg?branch=master)](https://coveralls.io/github/uncledu/genVersion?branch=master)
+
+```
+Usage: genVersion 
+
+Commands:
+  help show some example
+
+Options:
+  -h, --help  Show help                                                [boolean]
+
+Examples:
+  genVersion
+```
---
+++ src/bin.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+'use strict'
+import GenVersion from './index'
+import yargs from 'yargs'
+import child_process from 'child_process'
+
+let argv = yargs
+  .usage('Usage: $0')
+  .example(
+    '$0 ',
+    'auto set the version in package.json var current branch;if the branch name is daily/1.0.0,set the value 1.0.0'
+  )
+  .help('h')
+  .alias('h', 'help')
+  .global('[help]').argv
+
+try {
+  let genVersion = new GenVersion(argv)
+  genVersion()
+} catch (err) {
+  console.error(err)
+  process.exit(1)
+}
